4e894e96d810f919874865190814b941510ff923
Jacquelyn Elder <jacquelyn07@mac.com>
2021-04-23 17:36:14 -0500

    add authentication with passport

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const createError = require('http-errors');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const session = require('express-session');
+const passport = require('passport');
+const authenticate = require('./authenticate');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/usersRouter');
 const campsitesRouter = require('./routes/campsitesRouter');
 const promotionsRouter = require('./routes/promotionsRouter');
 const partnersRouter = require('./routes/partnersRouter');
-const session = require('express-session');
+
 const FileStore = require('session-file-store')(session);
 const mongoose = require('mongoose');
 
@@ -21,6 +22,14 @@ const connect = mongoose.connect(url, {
   useUnifiedTopology: true,
 });
 
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('./models/user');
+
+exports.local = passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 connect.then(
   () => console.log('Connected correctly to server'),
   (err) => console.log(err),
@@ -28,14 +37,12 @@ connect.then(
 
 const app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser('12345-67890-09876-54321'));
 
 app.use(
   session({
@@ -47,85 +54,30 @@ app.use(
   }),
 );
 
-function auth(req, res, next) {
-  console.log(req.session);
-
-  if (!req.session.user) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
-
-    const auth = Buffer.from(authHeader.split(' ')[1], 'base64')
-      .toString()
-      .split(':');
-    const user = auth[0];
-    const pass = auth[1];
-    if (user === 'admin' && pass === 'password') {
-      req.session.user = 'admin';
-      return next(); // authorized
-    } else {
-      const err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
-  } else {
-    if (req.session.user === 'admin') {
-      return next();
-    } else {
-      const err = new Error('You are not authenticated!');
-      err.status = 401;
-      return next(err);
-    }
-  }
-}
-
-app.use(auth);
-
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.use('/campsites', campsitesRouter);
 app.use('/promotions', promotionsRouter);
 app.use('/partners', partnersRouter);
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-function auth(req, res, next) {
-  console.log(req.session);
-
-  if (!req.session.user) {
-    const err = new Error('You are not authenticated!');
-    err.status = 401;
-    return next(err);
-  } else {
-    if (req.session.user === 'authenticated') {
-      return next();
-    } else {
-      const err = new Error('You are not authenticated!');
-      err.status = 401;
-      return next(err);
-    }
-  }
-}
-
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send({
+    line: 68,
+    status: err.status,
+    message: 'Caught by the error handling middleware',
+    err,
+  });
 });
 
 module.exports = app;
--- models/user.js
+++ models/user.js
@@ -1,22 +1,12 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  admin: {
-    type: Boolean,
-    default: false,
-  },
+const User = new Schema({
+  type: Boolean,
+  default: false,
 });
 
-const User = mongoose.model('User', userSchema);
+User.plugin(passportLocalMongoose);
 
-module.exports = User;
+module.exports = mongoose.model('User', User);
