cb38f927247ff323cbba7acec18e50e47fd40127
Jacquelyn Elder <jacquelyn07@mac.com>
2021-05-01 15:53:41 -0500

    Remove console.logs and commented code

--- routes/favoriteRouter.js
+++ routes/favoriteRouter.js
@@ -3,25 +3,8 @@ const Favorite = require('../models/favorite');
 const authenticate = require('../authenticate');
 const cors = require('./cors');
 const User = require('../models/user');
-const { response } = require('express');
 
 const favoriteRouter = express.Router();
-const authMiddleware = [authenticate.verifyUser, authenticate.verifyAdmin];
-
-function findMatches(firstArray, defaultArray) {
-  console.log({
-    firstArray,
-    defaultArray,
-  });
-  let sortedArray = [];
-  let values = firstArray.map((item) => Object.values(item));
-  values = values.flat();
-  console.log({ values });
-  values.map((first) => {
-    sortedArray[defaultArray.findIndex((def) => def === first)] = first;
-  });
-  return sortedArray.filter((v) => v._id);
-}
 
 favoriteRouter
   .route('/')
@@ -31,11 +14,6 @@ favoriteRouter
       .populate('user')
       .populate('campsites')
       .then((favorites) => {
-        console.log({
-          line: 'line 19',
-          method: 'Get /favorites',
-          favorites,
-        });
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
         res.json(favorites);
@@ -43,32 +21,9 @@ favoriteRouter
       .catch((err) => next(err));
   })
   .post(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
-    console.log({
-      userId: req.user._id,
-    });
     Favorite.findOne({ user: req.user._id }).then((favorite) => {
-      console.log({
-        favorite,
-      });
-      // const err = {
-      //   statusCode: 404,
-      //   message: `No Favorite found for this user ${req.user._id}`,
-      // };
-      // if (!favorite) {
-      //   next(err);
-      //   return;
-      // }
-      console.log({
-        line: '48',
-        favorite,
-      });
       if (favorite) {
-        console.log({ favorite });
         req.body.forEach((campsite) => {
-          console.log({
-            campsite,
-            favoriteIsNotIncluded: !favorite.campsites.includes(campsite._id),
-          });
           if (!favorite.campsites.includes(campsite._id)) {
             favorite.campsites.push(campsite._id);
             favorite.save().then((fav) => {
@@ -158,12 +113,8 @@ favoriteRouter
 
   .delete(cors.corsWithOptions, authenticate.verifyUser, (req, res, next) => {
     Favorite.findOne({ user: req.user._id }).then((favorite) => {
-      console.log({
-        favorite,
-      });
       if (favorite) {
         const index = favorite.campsites.indexOf(req.params.campsiteId);
-        console.log({ index });
         if (index >= 0) {
           favorite.campsites.splice(index, 1);
           favorite.save().then((fav) => {
@@ -178,7 +129,6 @@ favoriteRouter
           res.setHeader('Content-Type', 'text/plain');
           res.end('you do not have any favorites to delete');
         });
-        s;
       }
     });
   });
